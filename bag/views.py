3443d676070cc4816264063ca23aed60ac33188f
iKelvvv <kelvinlee@live.co.uk>
2022-04-10 17:58:25 +0000

    Add bag functionality to remove items with a button

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 # Create your views here.
@@ -37,7 +37,7 @@ def add_to_bag(request, item_id):
 
 
 def adjust_bag(request, item_id):
-    """Adjust quantity or remove specific item from the shopping bag"""
+    """Adjust quantity of items from the shopping bag"""
     
     quantity = int(request.POST.get('quantity'))
     weight = None
@@ -50,11 +50,36 @@ def adjust_bag(request, item_id):
             bag[item_id]['items_by_weight'][weight] = quantity
         else:
             del bag[item_id]['items_by_weight'][weight]
+            if not bag[item_id]['items_by_weight']:
+                bag.pop(item_id)
     else:
         if quantity > 0:
             bag[item_id] = quantity
         else:
-            bag.pop[item_id]
+            bag.pop(item_id)
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove items from the shopping bag"""
+    
+    try:
+        weight = None
+        if 'product_weight' in request.POST:
+            weight = request.POST['product_weight']
+        bag = request.session.get('bag', {})
+
+        if weight:
+            del bag[item_id]['items_by_weight'][weight]
+            if not bag[item_id]['items_by_weight']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
