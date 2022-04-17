23fbbc1fc58a5395ef3e19dd0f2e6169d9b11aba
iKelvvv <kelvinlee@live.co.uk>
2022-04-17 14:52:20 +0000

    Update checkout form for country field

--- checkout/forms.py
+++ checkout/forms.py
@@ -13,8 +13,8 @@ class OrderForm(forms.ModelForm):
             'street_address_2',
             'town_or_city',
             'postcode',
-            'country',
             'county',
+            'country',
         )
     
     def __init__(self, *args, **kwargs):
@@ -31,15 +31,15 @@ class OrderForm(forms.ModelForm):
             'town_or_city': 'Town or City',
             'postcode': 'Postcode',
             'county': 'County',
-            'country': 'Country',
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
