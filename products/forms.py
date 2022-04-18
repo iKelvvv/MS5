0b5912291a38c96ed1cb95937dda7bb441870b76
iKelvvv <kelvinlee@live.co.uk>
2022-04-18 08:54:50 +0000

    Add product admin form to template

--- products/forms.py
+++ products/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Product, Category
 
+
 class ProductForm(forms.ModelForm):
 
     class Meta:
@@ -10,6 +11,6 @@ class ProductForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
-        friendly_names = [(c.id, c.get_friendly_namee()) for c in categories]
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choices = friendly_names
