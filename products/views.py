7aa4f44549843801b689f5be3ebc749949d4e9db
iKelvvv <kelvinlee@live.co.uk>
2022-04-18 09:49:30 +0000

    Add functionality to edit and delete products

--- products/views.py
+++ products/views.py
@@ -75,9 +75,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Product has been added successfully!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product, Please check the form is valid.')
     else:
@@ -113,3 +113,13 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product has successfully deleted!')
+
+    return redirect(reverse('products'))
+
