f600665257a83dd26ca19a9e9491d40e68924a12
iKelvvv <kelvinlee@live.co.uk>
2022-04-18 12:58:53 +0000

    Remove secret and set debug to true only when in development env

--- SteakOut/settings.py
+++ SteakOut/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nrns^*k=*##b(p3o8)je_1hor09fe5v(d8n#heh$s4mu0ltqyq'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['kelvin-steakout.herokuapp.com', 'localhost']
 
