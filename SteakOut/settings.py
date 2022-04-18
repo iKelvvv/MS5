715c7d24b4376047aecaac1457076e52e0745661
iKelvvv <kelvinlee@live.co.uk>
2022-04-18 12:50:58 +0000

    Fix typo in database url in settings.py

--- SteakOut/settings.py
+++ SteakOut/settings.py
@@ -123,7 +123,7 @@ WSGI_APPLICATION = 'SteakOut.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ,get('DATABASE_URL')),
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL')),
     }
 else:
     DATABASES = {
