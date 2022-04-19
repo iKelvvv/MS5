cd9f7abc90dae42670e7f4307b768dfdbf74e370
iKelvvv <kelvinlee@live.co.uk>
2022-04-19 19:08:21 +0000

    Update message field to textfield in CustomerContact model

--- customer/models.py
+++ customer/models.py
@@ -9,4 +9,4 @@ class CustomerContact(models.Model):
     full_name = models.CharField(max_length=254, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     subject = models.CharField(max_length=50, null=False, blank=False)
-    message = models.CharField(max_length=254, null=False, blank=False)
+    message = models.TextField(max_length=254, null=False, blank=False)
