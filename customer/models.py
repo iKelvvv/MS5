c5fede37e084c973ddee17fd7f9cc680107e307d
iKelvvv <kelvinlee@live.co.uk>
2022-04-22 11:02:37 +0000

    Create model for newsletter subscriptions

--- customer/models.py
+++ customer/models.py
@@ -2,11 +2,20 @@ from django.db import models
 
 
 # Create your models here.
-from django.db import models
-
-# Create your models here.
 class CustomerContact(models.Model):
+    """ A Model for the customer contact form """
+
     full_name = models.CharField(max_length=254, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     subject = models.CharField(max_length=50, null=False, blank=False)
     message = models.TextField(max_length=254, null=False, blank=False)
+
+
+class NewletterSubscribers(models.Model):
+    """ A Model for the customer newsletter subscription form """
+
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
