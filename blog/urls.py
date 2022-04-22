072755c272b3dcbef02a647b401e5eaafe19eb5b
iKelvvv <kelvinlee@live.co.uk>
2022-04-22 13:32:20 +0000

    Add functionality and template to view blog post details

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blog, name='blog')
+    path('', views.blog, name='blog'),
+    path('<slug:post>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from .models import Post
 
 # Create your views here.
 def blog(request):
-
+    """ A view to see all published blog posts """
     all_posts = Post.objects.filter(status=1).order_by("-created_on")
 
     context = {
@@ -12,3 +12,16 @@ def blog(request):
     }
 
     return render(request, 'blog/blogs.html', context)
+
+
+
+def post_detail(request, post):
+    """ A view to see a single blog post """
+
+    post = get_object_or_404(Post, slug=post)
+
+    context = {
+        'post': post
+    }
+
+    return render(request, 'blog/post_detail.html', context)
